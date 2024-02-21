2004c350aa9c55a9e052d9553cf80e0156de5234
Etativel <farhanmaulana4611@gmail.com>
2024-02-21 11:58:50 +0700

    complete the game logic

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const startBtn = document.querySelector(".start-btn")
-
-
 const Board = (() => {
     let gameboard = ["", "", "", "", "", "", "", "", ""]
 
@@ -58,6 +55,9 @@ const Game = (() => {
     };
 
     const handleCLick = (e) => {
+        if (gameOver) {
+            return;
+        }
         let index = parseInt(e.target.id.split("-")[1])
        
         
@@ -66,18 +66,69 @@ const Game = (() => {
         }
 
         
+        
         Board.update(index, players[currentPlayerIndex].mark)
+        if(checkForWin(Board.getGameboard(), players[currentPlayerIndex].mark)){
+            gameOver = true;
+            console.log(`${players[currentPlayerIndex].name} Win!`)
+            // alert(`${players[currentPlayerIndex].name} Win!`)
+        } else if (checkForTie(Board.getGameboard())) {
+            gameOver = true;
+            // alert("Its a tie")
+            console.log("Its a tie")
+        }
+        
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
         
     }
 
+    const restart = () => {
+        gameOver = false;
+        for (let i = 0; i < 9; i++){
+            Board.update(i, "")
+        }
+        Board.render()
+    }
     return {
         start,
-        handleCLick
+        handleCLick,
+        restart
     }
 })();
 
-startBtn.addEventListener("click", ()=>{
+function checkForWin(board) {
+    console.log(board)
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (let i = 0; i < winningCombinations.length; i++){
+        const [a, b, c] = winningCombinations[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]){
+            return true
+        }
+    }
+    return false;
+}
+
+function checkForTie(board) {
+    return board.every(cell => cell !== "")
+}
+
+const restartButton = document.querySelector(".restart-btn")
+const startButton = document.querySelector(".start-btn")
+
+
+restartButton.addEventListener("click", ()=>{
+    Game.restart()
+})
+startButton.addEventListener("click", ()=>{
     Game.start()
 })
 
